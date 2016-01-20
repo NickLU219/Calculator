81aa278e71da2db8f2c7014dbaad584e49a4eb14
NickLU219 <nicolaslu94@gmail.com>
2016-01-20 11:39:59 +0800

    final commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var UserIsInTheMiddleOfInputANumber = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if UserIsInTheMiddleOfInputANumber{
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            UserIsInTheMiddleOfInputANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if UserIsInTheMiddleOfInputANumber {
+            enter()
+        }
+        switch operation {
+        case "×" :performOperation2 { $0 * $1 }
+        case "÷" :performOperation2 { $1 / $0 }
+        case "+" :performOperation2 { $0 + $1 }
+        case "−" :performOperation2 { $1 - $0 }
+        case "√" :performOperation1 { sqrt($0) }
+        default  :break
+        }
     }
+    
+    func performOperation2(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+        
+    }
+    func performOperation1(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+        
+    }
+    
+    
 
-
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        UserIsInTheMiddleOfInputANumber = false
+        operandStack.append(displayValue)
+        print("operandStack is \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            UserIsInTheMiddleOfInputANumber = false
+        }
+    }
+    
 }
 
